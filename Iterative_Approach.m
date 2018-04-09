a8b33d223c2a1fc2101926923f3af02b0539e644
Mholl-618 <mholl618@gmail.com>
2018-04-08 19:27:28 -0500

    2 Dof intro
    Added in that grav turn and 2 dof stuff

--- Iterative_Approach.m
+++ Iterative_Approach.m
@@ -1,7 +1,7 @@
 %Del Vee Comp Script
 clear all, clc
-del_v = 11000;
-f_one = linspace(.3,.6,100);
+del_v = 10750;
+f_one = .3;
 f_two = ones(1,length(f_one))-f_one;
 
 %% Top stage
@@ -9,42 +9,43 @@ f_two = ones(1,length(f_one))-f_one;
 go = 9.80665;
 ISP = 375; %assume vacuum performance
 ve = ISP*go;
-m_pay = 15000; %kg, aka 15 metric tons
+m_pay = 13000; %kg, aka 13 metric tons
 m_init_upper = [];
 m_final_upper = [];
 m_init_lower = [];
 m_final_lower = [];
 mprop_upper = [];
 mprop_lower = [];
-
+x = [0 0 0 0];
+del_t = .01;
+t = 0:del_t:550;
 
 for i = 1:length(f_two)
 
-    [mprop_upper(i),~,m_final_upper,m_init_upper(i)] = mass_vals(f_two(i)*del_v,375,.07,15000);
-    [mprop_lower(i),~,m_final_lower,m_init_lower(i)] = mass_vals(f_one(i)*del_v,350,.06,m_init_upper(i));
-    masses = [m_init_lower(i),mprop_lower(i),m_init_upper(i),mprop_upper(i)];
-%     tspan_lower = ((mprop_lower(i)*.99)/(495.2907320178509*7))+5;
-%     tspan_upper = ((mprop_upper(i)*.98)/516.6562)+1;
-%     t_cuts = [tspan_lower tspan_upper];
-%     tspan = 0:.01:sum(t_cuts);
-%     [T1{i},Y1{i}] = ode45(@(t,x)rocket_man(t,x,masses,t_cuts),tspan,[0 0],odeset('events',@altEvent));
-%     cell_convert = Y1{i};
-%     deltaVee_with_losses(i) = max(cell_convert(:,2));
+    [mprop_upper(i),~,m_final_upper,m_init_upper(i)] = mass_vals(f_two(i)*del_v,375,.08,13000);
+    [mprop_lower(i),~,m_final_lower,m_init_lower(i)] = mass_vals(f_one(i)*del_v,350,.08,m_init_upper(i));
+    masses = [m_init_lower(i) mprop_lower(i) m_init_upper(i) mprop_upper(i)];
+    m1_fi = mprop_lower(i);
+
+for m = 1:length(t)
+    [t(m),dx(m,:),masses(m+1,:),fuel_loss(m),q_act(m),fpa] = throttling_launch(t(m),x(m,:),masses(m,:),del_t,mprop_lower(i));
+    accel_x= dx(m,3);
+    accel_y= dx(m,4);
+    vel_x = x(m,3)+(accel_x*del_t);
+    vel_y = x(m,4)+(accel_y*del_t);
+    pos_x = x(m,1)+(vel_x*del_t);
+    pos_y = x(m,2)+(vel_y*del_t);
+    x(m+1,:) = [pos_x pos_y vel_x vel_y];
+end 
+
+    max_vels(i) = max(dx(:,1));
 end
-[M,I] = min(m_init_lower);
-% [hAx,hLine1,hLine2] = plotyy(f_two,m_init_lower,f_two,deltaVee_with_losses);
-% title('Mass and Delta V VS. F two');
-% ylabel(hAx(1),'Mass (kg)') % left y-axis 
-% ylabel(hAx(2),'Delta V with Losses (m/s)') % right y-axis
-% xlabel('F two')
 
-% figure
-% surf(f_two,m_init_lower,meshgrid(deltaVee_with_losses))
-
+[M,I] = max(max_vels);
 figure
 plot(f_two,m_init_lower)
 hold
-plot(f_two(I),M,'r*')
+plot(f_two(I),m_init_lower(I),'r*')
 xlabel('Stage 2 fractional \Delta V')
 ylabel('Total Launch Vehicle Mass (Kg)')
 title('Staging Optimization')
@@ -54,17 +55,26 @@ hold
 clc
 fprintf('Upper fuel mass %.2f \n',mprop_upper(I))
 fprintf('Lower fuel mass %.2f \n',mprop_lower(I))
-
+%%
+figure 
+plot(f_two,max_vels)
+hold
+plot(f_two(I),max_vels(I),'b*')
+xlabel('Stage 2 fractional \Delta V')
+ylabel('Delta V After Atmospheric Losses and Throttling')
+title('Staging Optimization')
 %% Prop Calcs
 upper_prop = mprop_upper(I);
 lower_prop = mprop_lower(I);
 m_init_upper = m_init_upper(I);
 m_init_lower = m_init_lower(I);
 
+masses = [m_init_lower,lower_prop,m_init_upper,upper_prop];
+
 rho_lox = 1141; %kg per m^3
 rho_lch4 = 422.62; %kg per m^3 
 
-stoch_ratio = 1.995; %grams of LO2 per grams of lch 4 
+stoch_ratio = 3.8; %grams of LO2 per grams of lch 4 
 
 
 lower_ch4 = lower_prop/2.995;
@@ -75,13 +85,13 @@ upper_lox = stoch_ratio*upper_ch4;
 
 m_dot_SLR = 1700e3/(350*go); %thrust  divided by isp and gravity
 m_dot_VR = 1900e3/(375*go); %Same as above, both ar Kg/s
-SA = (2.5908)^2*pi; %1 inch thick walls give us a safety factor of 
+SA = (1.80339999997968)^2*pi; %1 inch thick walls give us a safety factor of 
 vol_low_ch4 = lower_ch4/rho_lch4; % 4.12 in compressive yield failure, no clue on buckling
 vol_low_ox = lower_lox/rho_lox;
 vol_up_ch4 = upper_ch4/rho_lch4;
 vol_up_ox = upper_lox/rho_lox;
-hght_lower = (vol_low_ch4+vol_low_ox)/SA*1.05;
-hght_upper = (vol_up_ch4+vol_up_ox)/SA*1.05;
+hght_lower = (vol_low_ch4+vol_low_ox)/SA*1.1;
+hght_upper = (vol_up_ch4+vol_up_ox)/SA*1.1;
 total_hght = hght_lower+hght_upper; 
 tank_mass = pi*(1.8288^2-1.8034^2)*total_hght*2685; %2685 is the density of 2195 aluminum
 dome_mass = 4/3*pi*(1.8288^3-1.8034^3)*2685*.5;
@@ -98,54 +108,53 @@ upper_inert_mass = upper_tank_mass+upper_engine_mass+dome_per_stage;
 
 
 %% ODE TIMEEEEE
-pos_1 = [0];
-vel_1 = [0];
-tspan = 0:.1:460;
-masses = [m_init_lower,lower_prop,(m_init_upper),(upper_prop)];
+t = 0:.01:555;
+masses = zeros(length(t),4);
+masses = [m_init_lower,lower_prop,m_init_upper,upper_prop];
+dx = zeros(length(t),4);
+x = zeros(length(t),4);
+x(1,:) = [0 0 0 0];
+del_t = .01;
 
-% for n = 1:length(T1)
-%     dY = zeros(length(T1{n}),2);
-%     
-%     for i = 1:length(T1)
-%         dY(i,:) = rocket_man(T1(i),Y1(i,:),masses); 
-%     end
-% end
+x = [0 0 0 0];
 
- tspan_lower = ((lower_prop)/(495.2907320178509*7))+5;
-    tspan_upper = ((masses(4))/516.6562)+1;
-    t_cuts = [tspan_lower tspan_upper];
-    tspan = 0:.01:sum(t_cuts);
-    [T1,Y1] = ode45(@(t,x)rocket_man(t,x,masses,t_cuts),tspan,[0 0],odeset('events',@altEvent));
-for i = 1:length(T1)
-        dY(i,:) = rocket_man(T1(i),Y1(i,:),masses,t_cuts); 
+for i = 1:length(t)
+    [t(i),dx(i,:),masses(i+1,:),fuel_loss(i),q_act(i),fpa(i)] = throttling_launch(t(i),x(i,:),masses(i,:),del_t,mprop_lower(I));
+    accel_x= dx(i,3);
+    accel_y= dx(i,4);
+    vel_x = x(i,3)+(accel_x*del_t);
+    vel_y = x(i,4)+(accel_y*del_t);
+    pos_x = x(i,1)+(vel_x*del_t);
+    pos_y = x(i,2)+(vel_y*del_t);
+    x(i+1,:) = [pos_x pos_y vel_x vel_y];
 end
+%%
+plot(t,dx(:,3))
+figure
+plot(t,dx(:,4))
+figure
+plot(x(:,1),x(:,2))
+accel = sqrt(dx(:,3).^2+dx(:,4).^2);
+figure 
+plot(t,accel)
 
-q = [];
-drag = [];
-for i = 1:length(T1)
-        [rho,~,~,a,~] = std_atmosphere(Y1(i,1));
-        SA = (5.1816/2)^2*pi; %m^2 17 ft OD
-        cd = .075;
-        V = Y1(i,2);
-        q(i) = .5*rho*(V^2); 
-        drag(i) = q(i)*SA*cd;
-end
 
+%%
 
 figure
-[hAx,hLine1,hLine2] = plotyy(T1,((dY(:,2)+go)./go),T1,Y1(:,2));
+[hAx,hLine1,hLine2] = plotyy(t,dx(:,2)./go,t,dx(:,1)./go);
 title('Acceleration and Velocity VS. T+');
 ylabel(hAx(1),'Acceleration (g)') % left y-axis 
 ylabel(hAx(2),'Velocity (m/s)') % right y-axis
 xlabel('T+ (seconds)')
 % ylim([0 7]);
 figure
-plot(T1,q)
+plot(t,q_act)
 title('Dynamic Pressure vs T+')
 xlabel('T+ (seconds)')
 ylabel('Q (pascals)')
-[q_max, ind] = max(q);
-fprintf('Delta V with atm losses is %.2f \n',max(dY(:,1)))
+[q_max, ind] = max(q_act);
+fprintf('Delta V with atm losses is %.2f \n',max(dx(:,1)))
 fprintf('Max Q is %.2f pascals',q_max)
-fprintf(' at %.2f seconds \n',T1(ind))
+fprintf(' at %.2f seconds \n',t(ind))
 
---
+++ throttling_launch.m
@@ -0,0 +1,120 @@
+function [t,dx,masses,fuel_loss,q,fpa] = throttling_launch(t,x,masses,del_t,m1_fi)
+
+m1 = masses(1);
+m1_fuel = masses(2);
+m2 = masses(3);
+m2_fuel = masses(4);
+
+[rho,~,~,a,~] = std_atmosphere(x(2));
+        g = (6.6742e-11)*(5973600000000000000000000/((6371000+x(2))^2));
+        SA = (3.6576/2)^2*pi; %m^2 12 ft OD
+        cd = .075;
+        V = sqrt((x(3)^2)+(x(4)^2));
+        q = .5*rho*V^2;
+        MACH = V/a;
+        %Ascent Path Guidance  
+        
+        if x(2) < 10e3 %before grav turn alt
+            fpa = 1; %go straight up
+            var_pass = 0;
+        else %grav turn profile
+            var_pass = (tanh(3-(3*((150e3-x(2))/140e3))))*(pi/2); %final alt of 150
+            fpa = cos(var_pass);
+        end
+
+        
+        %Actual Math Part 
+        if m1_fuel > 10000 %1st Stage
+            if q < 28e3 ;
+                thrust = 1700e3*7;
+                accel_rocket = (thrust-(q*SA*cd))/m1;
+                if accel_rocket+(fpa*g) > 39.24
+                        accel_rocket = 4*9.81-(fpa*g);
+                        thrust = (m1*accel_rocket)+(fpa*g);
+                        fuel_loss = (thrust/(1700e3*7))*(495.3*7)*del_t;
+                else
+                    fuel_loss = 495.3*7*del_t;
+                end
+                
+            else 
+                thrust = (q*SA*cd)+(g*m1*fpa);
+                accel_rocket = 0;
+                fuel_loss = (thrust/(1700e3*7))*(495.3*7)*del_t;
+            end
+            m1 = m1 - fuel_loss;
+            m1_fuel = m1_fuel - fuel_loss;
+            masses(1) = m1;
+            masses(2) = m1_fuel;
+            thrust_x = sin(var_pass)*thrust;
+            thrust_y = fpa*thrust;
+            accel_x = thrust_x/m1;
+            accel_y = thrust_y/m1;
+            
+            if x(2) < 10e3 %basic vertical flight
+                dx(1) = x(3);
+                dx(2) = x(4);
+                dx(3) = accel_x;
+                dx(4) = accel_y-(fpa*g);
+            else 
+                r = x(2) + 6378e3; %orbital radius from center of earth
+                v_perp = x(3);
+                a_cent = (v_perp^2)/r; %positive (up)
+                dx(1) = x(3);
+                dx(2) = x(4);
+                dx(3) = accel_x;
+                dx(4) = accel_y+a_cent-(fpa*g);
+            end
+
+        else %2nd Stage
+            if m2_fuel > 3500 
+                thrust = 1900e3;
+                accel_rocket = (thrust - (q*SA*cd))/m2;
+                if (accel_rocket/9.81) + (fpa*g) > 4
+                        accel_rocket = 4*9.81-(fpa*g);
+                        thrust = (m2*accel_rocket)+(fpa*g);
+                        fuel_loss = (thrust/(1900e3))*(516.7)*del_t;
+                else
+                    fuel_loss = 516.6*del_t;
+                end
+            else 
+                fuel_loss = 0;
+                thrust = 0;
+                accel_rocket = 0;
+            end
+            m2 = m2 - fuel_loss;
+            m2_fuel = m2_fuel - fuel_loss;
+            masses(3) = m2;
+            masses(4) = m2_fuel;
+            thrust_x = sin(var_pass)*thrust;
+            thrust_y = fpa*thrust;
+            accel_x = thrust_x/m2;
+            accel_y = thrust_y/m2;
+            
+            if x(2) < 10e3 %basic vertical flight
+                dx(1) = x(3);
+                dx(2) = x(4);
+                dx(3) = accel_x;
+                dx(4) = accel_y-(fpa*g);
+            elseif m2_fuel > 3500 
+                r = x(2) + 6378e3; %orbital radius from center of earth
+                v_perp = x(3);
+                a_cent = (v_perp^2)/r; %positive (up)
+                dx(1) = x(3);
+                dx(2) = x(4);
+                dx(3) = accel_x;
+                dx(4) = accel_y+a_cent-(fpa*g);
+            else 
+                dx(1) = x(3);
+                dx(2) = x(4);
+                dx(3) = 0;
+                dx(4) = 0;
+            end
+    
+        end
+        
+        %basic orbit calcs
+
+
+    t = t;
+            
+end
